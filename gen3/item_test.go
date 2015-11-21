7f83626e7b9a9a0e8383a75a63a93f3b1059e65b
Anaminus <Anaminus@users.noreply.github.com>
2015-11-21 11:48:57 -0600

    [gen3] Add Item test.

---
+++ gen3/item_test.go
@@ -0,0 +1,41 @@
+package gen3_test
+
+import (
+	"github.com/anaminus/pkm/gen3"
+	"testing"
+)
+
+func TestItem(t *testing.T) {
+	ver := gen3.OpenROM(ROM(t))
+	if ver == nil {
+		t.Fatalf("failed to open ROM")
+	}
+
+	item := ver.ItemByIndex(1)
+	if v := item.Index(); v != 1 {
+		t.Errorf("Index: unexpected index %d", v)
+	}
+	if v := item.Name(); v != "MASTER BALL" {
+		t.Errorf("Name: unexpected name \"%s\"", v)
+	}
+	if v := item.Description(); v != "The best BALL that\ncatches a POKéMON\nwithout fail." {
+		t.Errorf("Description: unexpected description \"%s\"", v)
+	}
+	if v := item.Price(); v != 0 {
+		t.Errorf("Price: unexpected price %d", v)
+	}
+
+	item = ver.ItemByIndex(13)
+	if v := item.Index(); v != 13 {
+		t.Errorf("Index: unexpected index %d", v)
+	}
+	if v := item.Name(); v != "POTION" {
+		t.Errorf("Name: unexpected name \"%s\"", v)
+	}
+	if v := item.Description(); v != "Restores the HP of\na POKéMON by\n20 points." {
+		t.Errorf("Description: unexpected description \"%s\"", v)
+	}
+	if v := item.Price(); v != 300 {
+		t.Errorf("Price: unexpected price %d", v)
+	}
+}
